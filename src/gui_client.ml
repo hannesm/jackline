24368dc0f7f8a2bd1c0fe8f701bb4a0d9e99065b
Hannes Mehnert <hannes@mehnert.org>
2014-11-13 23:01:53 +0100

    some stuff

--- src/gui_client.ml
+++ src/gui_client.ml
@@ -1,3 +1,8 @@
+open Lwt
+
+let before_exit wake =
+  Printf.printf "just about to finish up\n%!" ;
+  Lwt.wakeup wake
 
 let () = Lwt_main.run (
     ignore (GMain.init ());
@@ -6,11 +11,26 @@ let () = Lwt_main.run (
     (* Thread which is wakeup when the main window is closed. *)
     let waiter, wakener = Lwt.wait () in
 
-    (* Create a window. *)
-    let window = GWindow.window () in
+    let window = GWindow.window ~width:320 ~height:240 ~title:"XMPP Client" () in
+    let vbox = GPack.vbox ~packing:window#add () in
 
-    (* Display something inside the window. *)
-    ignore (GMisc.label ~text:"Hello, world!" ~packing:window#add ());
+    (* Menu bar *)
+    let menubar = GMenu.menu_bar ~packing:vbox#pack () in
+    let factory = new GMenu.factory menubar in
+    let accel_group = factory#accel_group in
+    let menu = factory#add_submenu "Action" in
+
+    (* File menu *)
+    let factory = new GMenu.factory menu ~accel_group in
+    ignore (factory#add_item "Connect" ~callback:Xmpp.connect);
+    ignore (factory#add_item "Quit" ~key:GdkKeysyms._Q ~callback:(before_exit wakener));
+
+    (* Button *)
+    let button = GButton.button ~label:"Push me!" ~packing:vbox#add () in
+    ignore (button#connect#clicked ~callback:(fun () -> Printf.printf "blablabla\n%!"));
+
+    (* Display the windows and enter Gtk+ main loop *)
+    window#add_accel_group accel_group;
 
     (* Quit when the window is closed. *)
     ignore (window#connect#destroy (Lwt.wakeup wakener));
