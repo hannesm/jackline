85af26c61ae0bc9029d2c1996ca920c52095ba11
Hannes Mehnert <hannes@mehnert.org>
2014-11-15 01:51:19 +0100

    add top.ml

---
+++ top.ml
@@ -0,0 +1,7 @@
+(* #use this to setup the interactive environment. *)
+
+#require "erm_xmpp, otr, tls, tls.lwt, lwt, sexplib, sexplib.syntax";;
+
+#directory "_build/src";;
+#load "xmpp_client.cma";;
+
