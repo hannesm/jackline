01fadc216e5335d09d939b0d73ca227659882bdc
Hannes Mehnert <hannes@mehnert.org>
2014-11-27 21:06:36 +0100

    some stuff

--- src/cli_client.ml
+++ src/cli_client.ml
@@ -7,31 +7,37 @@ open LTerm_geom
 open CamomileLibraryDyn.Camomile
 open React
 
-let make_prompt size time =
+let make_prompt size time user status =
   let tm = Unix.localtime time in
-  let exit_code = 10 in
+(*  Printf.printf "\n\nblabla r%dc%d r%dc%d\n\n%!" size.rows size.cols tsize.rows tsize.cols ;
+  let matrix = LTerm_draw.make_matrix size in
+  let ctx = LTerm_draw.context matrix size in
+  LTerm_draw.clear ctx;
+  LTerm_draw.draw_frame ctx { row1 = 0; col1 = 0; row2 = size.rows; col2 = size.cols } LTerm_draw.Light; *)
 
   eval [
+    S "bla\n" ;
+    S(Zed_utf8.make (size.rows - 6) (UChar.of_int 0x0a));
     B_bold true;
 
     B_fg lcyan;
     S"─( ";
     B_fg lmagenta; S(Printf.sprintf "%02d:%02d" tm.Unix.tm_hour tm.Unix.tm_min); E_fg;
     S" )─< ";
-    B_fg lyellow; S "$"; E_fg;
+    B_fg lblue; S user; E_fg;
     S" >─";
     S(Zed_utf8.make
-        (size.cols - 23 - Zed_utf8.length "foo")
+        (size.cols - 22 - String.length user - String.length status)
         (UChar.of_int 0x2500));
     S"[ ";
-    B_fg(if exit_code = 0 then lwhite else lred); S "foo"; E_fg;
+    B_fg lred; S status; E_fg;
     S" ]─";
     E_fg;
     S"\n";
 
-    B_fg lred; S "user"; E_fg;
+(*    B_fg lred; S "user"; E_fg;
     B_fg lgreen; S"@"; E_fg;
-    B_fg lblue; S "domain"; E_fg;
+      B_fg lblue; S "domain"; E_fg; *)
     B_fg lgreen; S" $ "; E_fg;
 
     E_bold;
@@ -46,7 +52,7 @@ let time =
   ignore (Lwt_engine.on_timer 60.0 true (fun _ -> set_time (Unix.time ())));
   time
 
-class read_line ~term ~history ~completions = object(self)
+class read_line ~term ~history ~user ~completions = object(self)
   inherit LTerm_read_line.read_line ~history ()
   inherit [Zed_utf8.t] LTerm_read_line.term term
 
@@ -56,27 +62,26 @@ class read_line ~term ~history ~completions = object(self)
     self#set_completion 0 (List.map (fun f -> (f, " ")) completions)
 
   initializer
-    self#set_prompt (S.l2 (fun size time -> make_prompt size time) self#size time)
+    self#set_prompt (S.l2 (fun size time -> make_prompt size time user "offline") self#size time)
 end
 
-let rec loop term history =
+let rec loop term history config users =
   let completions = commands in
-  match_lwt
-    try_lwt
-      lwt command = (new read_line ~term ~history:(LTerm_history.contents history) ~completions)#run in
-      return (Some command)
-    with Sys.Break ->
-      return None
-  with
-    | Some command ->
-        Printf.printf "executing %s\n" command ;
+  let user = JID.string_of_jid config.Config.jid in
+  (match_lwt
+     try_lwt
+       lwt command = (new read_line ~term ~history:(LTerm_history.contents history) ~user ~completions)#run in
+       return (Some command)
+     with Sys.Break -> return None
+   with
+     | Some command ->
+         
+        Printf.printf "executing %s\n%!" command ;
         LTerm_history.add history command;
-        loop
-          term
-          history
-    | None ->
-        loop term history
-
+        return history
+     | None -> return history
+        ) >>= fun history ->
+  loop term history config users
 
 let () =
   Lwt_main.run (
@@ -93,7 +98,7 @@ let () =
     let history = LTerm_history.create [] in
     Lazy.force LTerm.stdout >>= fun term ->
     try_lwt
-      loop term history
+      loop term history config users
     with
       LTerm_read_line.Interrupt ->
         (
