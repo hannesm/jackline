70d205d6f705638de9f4da69e5af64e44473456b
Hannes Mehnert <hannes@mehnert.org>
2014-11-27 21:58:13 +0100

    log thingy

--- src/cli_client.ml
+++ src/cli_client.ml
@@ -7,17 +7,30 @@ open LTerm_geom
 open CamomileLibraryDyn.Camomile
 open React
 
-let make_prompt size time user status =
+let rec take_rev_fill x l acc =
+  match x, l with
+  | 0, _     -> acc
+  | n, x::xs -> take_rev_fill (pred n) xs (x::acc)
+  | n, []    -> take_rev_fill (pred n) [] ("no"::acc)
+
+let make_prompt size time log user status =
   let tm = Unix.localtime time in
 (*  Printf.printf "\n\nblabla r%dc%d r%dc%d\n\n%!" size.rows size.cols tsize.rows tsize.cols ;
   let matrix = LTerm_draw.make_matrix size in
   let ctx = LTerm_draw.context matrix size in
   LTerm_draw.clear ctx;
-  LTerm_draw.draw_frame ctx { row1 = 0; col1 = 0; row2 = size.rows; col2 = size.cols } LTerm_draw.Light; *)
+    LTerm_draw.draw_frame ctx { row1 = 0; col1 = 0; row2 = size.rows; col2 = size.cols } LTerm_draw.Light; *)
+  let logs = String.concat "\n" (take_rev_fill 6 log []) in
 
   eval [
     S "bla\n" ;
-    S(Zed_utf8.make (size.rows - 6) (UChar.of_int 0x0a));
+    S(Zed_utf8.make
+        (size.rows - 6 (* log *) - 3 (* status + readline + ^ 'bla' *))
+        (UChar.of_int 0x0a));
+
+    S logs ;
+    S "\n" ;
+
     B_bold true;
 
     B_fg lcyan;
@@ -37,8 +50,8 @@ let make_prompt size time user status =
 
 (*    B_fg lred; S "user"; E_fg;
     B_fg lgreen; S"@"; E_fg;
-      B_fg lblue; S "domain"; E_fg; *)
-    B_fg lgreen; S" $ "; E_fg;
+      B_fg lblue; S "domain"; E_fg;
+      B_fg lgreen; S" $ "; E_fg; *)
 
     E_bold;
   ]
@@ -52,7 +65,7 @@ let time =
   ignore (Lwt_engine.on_timer 60.0 true (fun _ -> set_time (Unix.time ())));
   time
 
-class read_line ~term ~history ~user ~completions = object(self)
+class read_line ~log ~term ~history ~user ~completions = object(self)
   inherit LTerm_read_line.read_line ~history ()
   inherit [Zed_utf8.t] LTerm_read_line.term term
 
@@ -61,27 +74,28 @@ class read_line ~term ~history ~user ~completions = object(self)
     let completions = List.filter (fun f -> Zed_utf8.starts_with f prefix) completions in
     self#set_completion 0 (List.map (fun f -> (f, " ")) completions)
 
+  method show_box = false
+
   initializer
-    self#set_prompt (S.l2 (fun size time -> make_prompt size time user "offline") self#size time)
+    self#set_prompt (S.l2 (fun size time -> make_prompt size time log user "offline") self#size time)
 end
 
-let rec loop term history config users =
+let rec loop term log history config users =
   let completions = commands in
   let user = JID.string_of_jid config.Config.jid in
   (match_lwt
      try_lwt
-       lwt command = (new read_line ~term ~history:(LTerm_history.contents history) ~user ~completions)#run in
+       lwt command = (new read_line ~log ~term ~history:(LTerm_history.contents history) ~user ~completions)#run in
        return (Some command)
      with Sys.Break -> return None
    with
      | Some command ->
-         
         Printf.printf "executing %s\n%!" command ;
         LTerm_history.add history command;
-        return history
-     | None -> return history
-        ) >>= fun history ->
-  loop term history config users
+        return (command, history)
+     | None -> return ("", history)
+  ) >>= fun (li, history) ->
+  loop term (li::log) history config users
 
 let () =
   Lwt_main.run (
@@ -98,7 +112,7 @@ let () =
     let history = LTerm_history.create [] in
     Lazy.force LTerm.stdout >>= fun term ->
     try_lwt
-      loop term history config users
+      loop term [] history config users
     with
       LTerm_read_line.Interrupt ->
         (
