091e8c78e070daf635e77982f8226b7ded2a3ab7
Hannes Mehnert <hannes@mehnert.org>
2014-12-04 18:50:48 +0100

    refactor to a single file

---
+++ cli/cli_state.ml
@@ -0,0 +1,18 @@
+
+type ui_state = {
+  user : User.user ; (* set initially *)
+  session : User.session ; (* set initially *)
+  mutable log : (Unix.tm * string * string) list ; (* set by xmpp callbacks -- should be time * string list *)
+  mutable active_chat : (User.user * User.session option) ; (* modified by user (scrolling through buddies) *)
+  users : User.users ; (* extended by xmpp callbacks *)
+  mutable notifications : User.user list ;
+}
+
+let empty_ui_state user session users = {
+  user ;
+  session ;
+  log = [] ;
+  active_chat = (user, Some session) ;
+  users ;
+  notifications = []
+}
