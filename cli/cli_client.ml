574442d76e45aa591e43d42eae27158990ff064d
user@ocamldev <none>
2014-12-05 16:02:34 +0100

    line_wrap: (tested:) fix busyloop off-by-one + add newline compaction

--- cli/cli_client.ml
+++ cli/cli_client.ml
@@ -65,11 +65,13 @@ let make_prompt size time network state redraw =
   let rec line_wrap ~max_length ent acc : string list =
     match ent with
     | entry::remaining when String.contains entry '\n' ->
-      let part1 = String.sub entry 0 (String.index entry '\n') in
-      let part1_len = String.length part1 in
-      let part2 = "  " ^ String.sub entry part1_len ((String.length entry) - part1_len)
+      let part1     = String.sub entry 0 (String.index entry '\n') in
+      let part1_len = 1 + String.length part1 in (* +1: account for \n *)
+      let part2    = "  " ^ String.sub entry part1_len ((String.length entry) - part1_len) in
+      let acc       = if 0 <> String.length (String.trim part1) then (part1::acc) else acc in
+      let remaining = if 0 <> String.length (String.trim part2) then (part2::remaining) else remaining
       in
-        line_wrap ~max_length (part2::remaining) (part1::acc)
+          line_wrap ~max_length remaining acc
     | entry::remaining when (String.length entry) > max_length ->
         let part1 = String.sub entry 0 max_length
         and part2 = "  " ^ String.sub entry max_length ((String.length entry) - max_length)
