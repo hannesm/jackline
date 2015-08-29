a40f0af7031e90c1cb69fc9be97ed0acc736b65f
Hannes Mehnert <hannes@mehnert.org>
2015-08-29 17:48:03 +0100

    install only xmpp_client and xmpp_client_cli (both packed) remove unneeded binding
    thanks to @dbuenzli for raising this issue

--- pkg/build.ml
+++ pkg/build.ml
@@ -2,20 +2,9 @@
 #directory "pkg";;
 #use "topkg.ml";;
 
-let cmdliner = Env.bool "cmdliner"
-
 let () = Pkg.describe "jackline" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
-    Pkg.lib ~exts:Exts.library "src/config";
-    Pkg.lib ~exts:Exts.library "src/escape";
-    Pkg.lib ~exts:Exts.library "src/persistency";
-    Pkg.lib ~exts:Exts.library "src/user";
-    Pkg.lib ~exts:Exts.library "src/utils";
-    Pkg.lib ~exts:Exts.library "src/xmpp_callbacks";
-    Pkg.lib ~exts:Exts.library "src/xmpp_connection";
-    Pkg.lib ~exts:Exts.library "cli/cli_client";
-    Pkg.lib ~exts:Exts.library "cli/cli_commands";
-    Pkg.lib ~exts:Exts.library "cli/cli_config";
-    Pkg.lib ~exts:Exts.library "cli/cli_state";
+    Pkg.lib ~exts:[".cmo"] "src/xmpp_client";
+    Pkg.lib ~exts:[".cmo"] "cli/xmpp_client_cli";
     Pkg.bin ~auto:true "bin/jackline";
     Pkg.doc "README.md"; ]
