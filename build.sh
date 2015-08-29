146e2d5f597f4456ea1f69daaa1305fcce4ef641
Hannes Mehnert <hannes@mehnert.org>
2015-08-29 17:57:57 +0100

    add local build

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# This script is only used for developement. It is removed by the
+# distribution process.
+
+set -e
+
+OCAMLBUILD=${OCAMLBUILD:="ocamlbuild -tag debug -classic-display \
+                          -use-ocamlfind" }
+
+action ()
+{
+    case $1 in
+        default) action lib;;
+        lib) $OCAMLBUILD bin/jackline.native ;;
+        clean)   $OCAMLBUILD -clean ;;
+        *)       $OCAMLBUILD $* ;;
+    esac
+}
+
+if [ $# -eq 0 ];
+then action default ;
+else action $*; fi
