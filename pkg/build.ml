a5cfc8c0d059116a0dd2025c8c58253e501dd531
Hannes Mehnert <hannes@mehnert.org>
2015-08-29 18:51:25 +0100

    no need to install any library

--- pkg/build.ml
+++ pkg/build.ml
@@ -4,7 +4,5 @@
 
 let () = Pkg.describe "jackline" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
-    Pkg.lib ~exts:[".cmo"] "src/xmpp_client";
-    Pkg.lib ~exts:[".cmo"] "cli/xmpp_client_cli";
     Pkg.bin ~auto:true "bin/jackline";
     Pkg.doc "README.md"; ]
