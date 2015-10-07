0e50d334e245e9986c4b0fd3742e28c1d6536f96
Hannes Mehnert <hannes@mehnert.org>
2015-10-07 16:04:24 +0100

    provide a low resolution timestamp function (resolution = day)

--- src/utils.ml
+++ src/utils.ml
@@ -2,4 +2,9 @@ let option none some = function
   | None   -> none
   | Some x -> some x
 
+let today () =
+  let now = Unix.time () in
+  let day = 24 * 60 * 60 in
+  Int64.(to_float (sub (of_float now) (rem (of_float now) (of_int day))))
+
 let version = "%%VERSION%%"
