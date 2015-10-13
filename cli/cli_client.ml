a973a54af83cb84a278b44691ac7240a2b0a0579
Hannes Mehnert <hannes@mehnert.org>
2015-10-13 17:12:31 +0100

    add back surrounding -

--- cli/cli_client.ml
+++ cli/cli_client.ml
@@ -262,7 +262,7 @@ let horizontal_line buddy resource fg_color buddy_width scrollback show_buddy_li
           " - no OTR "
           (fun fp ->
            let vs = User.verified_fp user fp in
-           " " ^ User.verification_status_to_string vs ^ " ")
+           " - " ^ User.verification_status_to_string vs ^ " - ")
           (User.otr_fingerprint s.User.otr)
     | _ -> "")
   in
